25b72793bcb9ca2f124d388056902754d5d7ef47
dragonruler1000 <github@me.minecraftchest2.us>
2025-07-02 12:28:00 -0500

    Add timer and progress bar functionality to levels; update UI elements

--- UI stuff/scenes/game_scene/levels/level.gd
+++ UI stuff/scenes/game_scene/levels/level.gd
@@ -2,6 +2,10 @@ extends Node
 
 signal level_won
 signal level_lost
+@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var timer: Timer = $BombTimer
+@onready var time_label: Label = $TimeLabel
+var max_time := 10.0  # in seconds
 
 var level_state : LevelState
 
@@ -21,6 +25,18 @@ func _ready() -> void:
 	%BackgroundColor.color = level_state.color
 	if not level_state.tutorial_read:
 		open_tutorials()
+	# Set Timer properties
+	timer.wait_time = max_time
+	timer.one_shot = true
+	timer.start()
+
+	# Set ProgressBar range
+	progress_bar.min_value = 0
+	progress_bar.max_value = max_time
+	progress_bar.value = max_time
+
+	# Update bar every frame
+	set_process(true)
 
 func _on_color_picker_button_color_changed(color : Color) -> void:
 	%BackgroundColor.color = color
@@ -29,3 +45,17 @@ func _on_color_picker_button_color_changed(color : Color) -> void:
 
 func _on_tutorial_button_pressed() -> void:
 	open_tutorials()
+	
+func _on_bomb_timer_timeout() -> void:
+	level_lost.emit()
+
+func _process(_delta):
+	if timer.time_left > 0:
+		progress_bar.value = timer.time_left
+		time_label.text = "%.1fs" % timer.time_left  # shows 1 decimal place like "9.2s"
+		if timer.time_left <= 3.0:
+			time_label.modulate = Color.RED
+		else:
+			time_label.modulate = Color.WHITE
+	else:
+		progress_bar.value = 0
